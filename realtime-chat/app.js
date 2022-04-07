b76e4ea7bf074dd58043800b7ab4fec373553331
louis-25 <20151577@vision.hoseo.edu>
2022-04-07 23:40:57 +0900

    NodeJS로 실시간 채팅서버 구현
    ngrok http 5000 명령어로 간단히 배포해볼 수 있음

---
+++ realtime-chat/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+const moment = require("moment");
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "src")));
+
+const PORT = process.env.PORT || 5000;
+
+io.on("connection", (socket) => {
+  //chatting id로 연결된 소켓에서 전달받은 데이터
+  socket.on("chatting", (data) => {
+    const { name, msg } = data;
+    //연결된 소켓에 data전송
+    io.emit("chatting", {
+      name,
+      msg,
+      time: moment(new Date()).format("h:ss A"),
+    });
+  });
+});
+
+server.listen(PORT, () => console.log(`server is running ${PORT}`));
