0cda2f618477046b5616dd05d09cacaf3d39ee9d
louis-25 <20151577@vision.hoseo.edu>
2022-05-25 16:09:39 +0900

    메세지 신고기능 추가

--- ttja_chat_server/app.js
+++ ttja_chat_server/app.js
@@ -27,19 +27,19 @@ io.on("connection", async (socket) => {
 
   console.log('socket connected!',socket.id);    
   // console.log(socket);
-  socket.on('joinRoom', (roomName) => {
-    let room = roomName;
+  socket.on('joinRoom', (roomId) => {
+    let room = roomId;
     socket.join(room);    
     console.log("join room",room);
   })
-  socket.on('leaveRoom', (roomName) => {
-    let room = roomName;
+  socket.on('leaveRoom', (roomId) => {
+    let room = roomId;
     socket.leave(room);    
     console.log(room,'room 접속 해제');
   })
   socket.on('deleteMsg', async (seq) => {     
     try {
-      await model.deleteChatData(seq); // seq에 해당되는 메세지 DB에서 제거
+      await model.deleteChatData(seq); // seq에 해당되는 메세지 DB에서 제거 및 STATUS D로 변경
       io.emit("deleteMsg", seq);
       console.log(seq,"번 메세지 제거");
     }catch(e) {
@@ -48,13 +48,18 @@ io.on("connection", async (socket) => {
   })
   socket.on("report", async (data) => {
     const { seq, status, member_id } = data;
+    console.log('data', data);
     await model.setChatReport(seq, status, member_id);
     let getChatSeq = await model.getChatSeq(seq);
-    if(status == "Y") {
+    if(status == "B") { // 차단
       io.emit("report", getChatSeq);
       await model.setChatReportHistory(seq);
       console.log(seq,'번 데이터 차단됨');
-    }else if(status == "D") {
+    }else if(status == "R") { // 신고
+      io.emit("report", getChatSeq);
+      await model.setChatReportHistory(seq);
+      console.log(seq,'번 데이터 해제됨');
+    }else if(status == "B2") { // 차단 해제
       io.emit("report", getChatSeq);
       await model.setChatReportHistory(seq);
       console.log(seq,'번 데이터 해제됨');
@@ -62,7 +67,7 @@ io.on("connection", async (socket) => {
   })
   //chatting id로 연결된 소켓에서 전달받은 데이터
   socket.on("chatting", async (data) => {
-    let { memberId, memberNick, memberImg, msg, time, roomName } = data;
+    let { memberId, memberNick, memberImg, msg, time, roomId } = data;
     console.log('id: ',memberId);
     // if(memberId == "") {memberId = "33119";}
     console.log('name: ',memberNick);
@@ -70,11 +75,11 @@ io.on("connection", async (socket) => {
     if(memberImg == "") {memberImg = "none.png";}
     console.log('msg: ',msg);
     console.log('time: ',time);
-    console.log('roomName: ',roomName);
+    console.log('roomId: ',roomId);
     
     try {
-      socket.join(roomName); // room에 연결
-      await model.setChatData(memberId, memberNick, memberImg, msg, roomName, time);
+      socket.join(roomId); // room에 연결
+      await model.setChatData(memberId, memberNick, memberImg, msg, roomId, time);
       if(msg=="모든Room에 송출") {
         io.emit('broadcast', {
           memberId,
@@ -87,7 +92,7 @@ io.on("connection", async (socket) => {
       // const rowData = await model.getUser();
       const rowData = await model.getChatOne(memberId);
       console.log('rowData',rowData);
-      io.to(roomName).emit('broadcast', {
+      io.to(roomId).emit('broadcast', {
         seq: rowData[0].SEQ,
         memberId: rowData[0].MEMBER_ID,
         memberNick: rowData[0].NAME,
@@ -101,8 +106,8 @@ io.on("connection", async (socket) => {
     }
     
     socket.on('disconnect', ()=>{
-      console.log(roomName,'room 접속 해제');
-      socket.leave(roomName);
+      console.log(roomId,'room 접속 해제');
+      socket.leave(roomId);
     })
 
     // //연결된 소켓에 data전송
--- ttja_chat_server/models/chat_model.js
+++ ttja_chat_server/models/chat_model.js
@@ -36,7 +36,8 @@ class models {
   }
   async getChatOne(memberId) { // 해당 멤버의 최신 데이터 하나 반환
     // console.log(memberId);
-    sqlQuery = `SELECT MAX(SEQ), MEMBER_ID, NAME, IMG, MSG, ROOM_NAME, DATE_FORMAT(REG_DATE, "%Y-%m-%d %H:%i:%s") AS REG_DATE, STATUS FROM TB2_CHAT_DATA WHERE MEMBER_ID="${memberId}";`;
+    sqlQuery = `SELECT SEQ, MEMBER_ID, NAME, IMG, MSG, ROOM_ID, DATE_FORMAT(REG_DATE, "%Y-%m-%d %H:%i:%s") AS REG_DATE, STATUS FROM TB2_CHAT_DATA 
+                WHERE SEQ = (SELECT MAX(SEQ) FROM TB2_CHAT_DATA WHERE MEMBER_ID="${memberId}");`;
     
     const query = new Promise((resolve, reject)=>{
       connection.query(sqlQuery, async (err, result, fields) => {      
@@ -51,7 +52,7 @@ class models {
     return await Promise.resolve(result);
   }
   async getChatSeq(seq) { // Seq에 일치하는 데이터 반환    
-    sqlQuery = `SELECT SEQ, MEMBER_ID, NAME, IMG, MSG, ROOM_NAME, DATE_FORMAT(REG_DATE, "%Y-%m-%d %H:%i:%s") AS REG_DATE, STATUS, REPORT_MEMBER_ID 
+    sqlQuery = `SELECT SEQ, MEMBER_ID, NAME, IMG, MSG, ROOM_ID, DATE_FORMAT(REG_DATE, "%Y-%m-%d %H:%i:%s") AS REG_DATE, STATUS, REPORT_MEMBER_ID 
                 FROM TB2_CHAT_DATA WHERE SEQ=${seq}`;
     
     const query = new Promise((resolve, reject)=>{
@@ -66,10 +67,10 @@ class models {
     const result = await query;
     return await Promise.resolve(result);
   }
-  async setChatData(memberId, memberNick, memberImg, msg, roomName, time){
+  async setChatData(memberId, memberNick, memberImg, msg, roomId, time){
     console.log('test',time);
-    sqlQuery = `INSERT INTO TB2_CHAT_DATA(MEMBER_ID, NAME, IMG, MSG, ROOM_NAME, REG_DATE)
-                VALUES(${memberId}, '${memberNick}', '${memberImg}', '${msg}', '${roomName}', '${time}');`
+    sqlQuery = `INSERT INTO TB2_CHAT_DATA(MEMBER_ID, NAME, IMG, MSG, ROOM_ID, REG_DATE)
+                VALUES(${memberId}, '${memberNick}', '${memberImg}', '${msg}', '${roomId}', '${time}');`
 
     
     connection.query(sqlQuery, (err, result, fields) => {
@@ -80,11 +81,11 @@ class models {
     });
     return await Promise.resolve(this.result);
   }
-  async setChatReport (seq, status, member_id){
-    if(status == 'Y') {
-      sqlQuery = `UPDATE TB2_CHAT_DATA SET STATUS = 'D', REPORT_MEMBER_ID = '${member_id}' WHERE SEQ = ${seq}`
-    }else if(status == 'D') {
-      sqlQuery = `UPDATE TB2_CHAT_DATA SET STATUS = 'Y', REPORT_MEMBER_ID = '0' WHERE SEQ = ${seq}`
+  async setChatReport (seq, status, member_id){    
+    if(status == "B2") { // 차단해제
+      sqlQuery = `UPDATE TB2_CHAT_DATA SET STATUS = 'Y', REPORT_MEMBER_ID = '0' WHERE SEQ = ${seq}`;  
+    }else {
+      sqlQuery = `UPDATE TB2_CHAT_DATA SET STATUS = '${status}', REPORT_MEMBER_ID = '${member_id}' WHERE SEQ = ${seq}`;
     }
 
     connection.query(sqlQuery, (err, result, fields) => {
@@ -97,8 +98,8 @@ class models {
   }
   async setChatReportHistory (seq){ // 신고,차단 기록하기
     
-    sqlQuery = `INSERT INTO TB2_CHAT_REPORT_HISTORY(SEQ, MEMBER_ID, NAME, IMG, MSG, ROOM_NAME, REG_DATE, STATUS, REPORT_MEMBER_ID) 
-                SELECT * FROM TB2_CHAT_DATA WHERE SEQ=${seq}`;
+    sqlQuery = `INSERT INTO TB2_CHAT_REPORT_HISTORY(SEQ, ROOM_ID, MSG, STATUS, REPORT_MEMBER_ID)
+                SELECT SEQ, ROOM_ID, MSG, STATUS, REPORT_MEMBER_ID FROM TB2_CHAT_DATA WHERE SEQ=${seq}`;
 
     connection.query(sqlQuery, (err, result, fields) => {
       if(err){
@@ -108,8 +109,8 @@ class models {
     });
     return await Promise.resolve(this.result);
   }
-  async deleteChatData(seq){ // seq에 해당되는 데이터 한줄 제거
-    sqlQuery = `DELETE FROM TB2_CHAT_DATA WHERE SEQ=${seq}`;
+  async deleteChatData(seq){ // seq에 해당되는 데이터의 상태를 D로 변경
+    sqlQuery = `UPDATE TB2_CHAT_DATA SET STATUS = 'D' WHERE SEQ=${seq}`;
 
     connection.query(sqlQuery, (err, result, fields) => {
       if(err){
